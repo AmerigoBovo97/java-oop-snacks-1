8a58c03f1c2c66f40b7d4a424d08e436238bcac8
AmerigoBovo97 <amerigobovo@gamil.com>
2024-11-21 20:44:35 +0100

    feat: built Studente class

--- src/Studente.java
+++ src/Studente.java
@@ -1,3 +1,17 @@
 public class Studente {
     
+
+    private String name;
+    private String surname;
+    private int age;
+
+    public Studente (String name, String surname, int age){
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public String getDetails(){
+        return this.name + "" + this.surname + "" + this.age;
+    }
 }
